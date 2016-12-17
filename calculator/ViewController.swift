fc7f2a35384c65ad72012ec2c785022440cc0e81
Apple <apple@AppletekiMacBook-Air-4.local>
2016-12-17 22:40:55 +0800

    Here is my commit message

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var peopleField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var sharetipLabel: UILabel!
+    @IBOutlet weak var sharetotalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+  
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.15,0.20,0.22]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let people = Double(peopleField.text!) ?? 1
+        let shareTip = tip / people
+        let shareTotal = total / people
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        sharetipLabel.text = String(format: "$%.2f", shareTip)
+        sharetotalLabel.text = String(format: "$%.2f", shareTotal)
+    }
+    
 }
 
